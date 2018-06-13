5abb3ba91044093f80afd25f047c05414a02b630
Kolby <kolbykalafut@gmail.com>
2018-06-13 17:18:01 -0400

    Redis reconfig again

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,1 @@
-# uri = URI.parse("redis://redistogo:de9961cc6950ac6c94ab14396ed2303a@cod.redistogo.com:11141/")
-# $redis = Redis.new(:host => uri.host, :port => uri.port)
-# Resque.redis = $redis
+$redis = Redis.new(url: "redis://redistogo:de9961cc6950ac6c94ab14396ed2303a@cod.redistogo.com:11141/")
