3fff0fdcbac0984b47e76804a10e8bf8298ce873
Kolby <kolbykalafut@gmail.com>
2018-06-25 20:31:43 -0400

    g

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,13 @@ module PdfConverter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL',
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Methods' => 'GET, PUT, OPTIONS POST'
+    }
+
   end
 end
