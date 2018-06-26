60448276946c4b3a88729834e5090b5292fbacc5
Kolby <kolbykalafut@gmail.com>
2018-06-26 16:21:05 -0400

    Changed .json over for client

--- lib/pdf_converter.rb
+++ lib/pdf_converter.rb
@@ -10,12 +10,12 @@ class PageTextReceiver
   else
     base_uri 'http://localhost:3000'
   end
-  
+
   def index(value)
     value
   end
   def run(file)
-    response = PageTextReceiver.post('https://pdftables.com/api?key=nnu16oof0hsn&format=csv', :query => { f: File.new("#{file}", "rb") })
+    response = PageTextReceiver.post('https://pdftables.com/api?key=6z9xogtmgplz&format=csv', :query => { f: File.new("#{file}", "rb") })
     index = self.index(@params[:index])
     File.open("response_#{self.index(@params[:index])}.csv", 'w') do |f|
       f.puts response.body
